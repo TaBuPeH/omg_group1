36676eb23838f9731b58239a00b02f72fd588952
Angel Mizinov <b.rabbit.b@abv.bg>
2016-02-22 23:31:35 +0200

    HW Lesson11 Angel

---
+++ lesson11/homework/Angel/index.php
@@ -0,0 +1,64 @@
+<?php
+echo "Hello!";
+
+	
+$cars = Array(
+   
+   		0=> Array(
+			"descriptionOfTheProduct" => Array(
+				'brand'=>'Audi' => Array(
+					'model'=> 'A4',
+					'model'=> 'A6',
+					'model'=> 'A8'
+					),
+				'year'=> 2014,
+			),
+			"price" => Array(
+				'price1' => 30 000,
+				'price2' => 50 000,
+				'price3' => 75 000
+				),
+			"quantity" => 3
+		),
+		
+		
+		1=> Array(
+			"descriptionOfTheProduct" => Array(
+				'brand'=>'BMW' => Array(
+					'model'=> '320d',
+					'model'=> '520d',
+					'model'=> '730',
+					'model'=> 'X6'
+					),
+				'year'=> 2013,
+			),
+			"price" => Array(
+				'price1' => 20 000,
+				'price2' => 30 000,
+				'price3' => 45 000,
+				'price4' => 100 000
+				),
+			"quantity" => 4
+		),
+
+		2=> Array(
+			"descriptionOfTheProduct" => Array(
+				'brand'=>'Mercedes-Benz' => Array(
+					'model'=> 'CLS 350',
+					'model'=> 'S 500',
+					'model'=> 'C 200'
+					),
+				'year'=> 2015,
+			),
+			"price" => Array(
+				'price1' => 80 000,
+				'price2' => 50 000,
+				'price3' => 15 000
+				),
+			"quantity" => 3
+		)
+   );
+
+
+
+?>
